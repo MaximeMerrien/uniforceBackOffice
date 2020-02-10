86736dd4327c9297d16b0ddd66fd1e9c5156e8cf
MaximeMerrien <maxime.merrien@imie.fr>
2020-02-10 13:23:33 +0100

    [FIX] Correction d'une erreur de compilation en raison de tags mal fermés

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
-import { HydraAdmin } from "@api-platform/admin";
+import { HydraAdmin, ResourceGuesser } from "@api-platform/admin";
 
 // Replace with your own API entrypoint
 // For instance if https://example.com/api/books is the path to the collection of book resources,
 // then the entrypoint is https://example.com/api
 export default () => ( 
-  <HydraAdmin entrypoint="http://localhost:8000/api"></HydraAdmin> 
+  <HydraAdmin entrypoint="http://localhost:8000/api">
+  	{/* Ressources visibles sur le back-office */}
+  	<ResourceGuesser name="materiels" />
+  	<ResourceGuesser name="evenements" />
+  	<ResourceGuesser name="actualites" />
+  	<ResourceGuesser name="palmares" />
+  </HydraAdmin>
 );
